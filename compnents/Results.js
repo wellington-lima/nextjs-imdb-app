f7002715dc65e018a2f70bba4161f8d4180ccedb
Wellington <wlima2santos@gmail.com>
2023-10-24 11:59:31 -0300

    create components

---
+++ compnents/Results.js
@@ -0,0 +1,8 @@
+export default function Results() {
+  return(
+    <div>
+      <h1>Results</h1>
+    </div>  
+  
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,23 @@
+import Head from "next/head"
+import Header from "../compnents/Header"
+import Navbar from "../compnents/Navbar"
+import Results from "../compnents/Results"
 
 export default function Home() {
   return (
-    <h1>Index App</h1>
+    <div>
+      <Head>
+        <title>App</title>
+        <meta name="description"></meta>
+        <link rel="icon" href="/favicon"></link>
+      </Head>
+
+      <Header />
+
+      <Navbar />
+      
+      <Results />
+
+    </div>
   )
 }
