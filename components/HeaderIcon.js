e193d5150960879e04e6eded905a3bc0ca4f0d3f
Wellington <wlima2santos@gmail.com>
2023-10-27 11:26:13 -0300

    style the header section

--- components/HeaderIcon.js
+++ components/HeaderIcon.js
@@ -1,10 +1,8 @@
-import { BeakerIcon } from '@heroicons/react/24/solid'
-
 export default function HeaderIcon({Icon, title}) {
   return(
-    <div>
+    <div className="mx-4 flex flex-col cursor-pointer hover:text-white active:text-red-400 lg:mx-6">
       <Icon className="h-8" />
-      <p>{title}</p>
+      <p className="my-2">{title}</p>
     </div>
   )
 }
