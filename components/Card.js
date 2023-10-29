462942e73c821422fd7d17d4282320c10c0a6a74
Wellington <wlima2santos@gmail.com>
2023-10-28 21:56:33 -0300

    complete the card component and make the result section responsive

--- components/Card.js
+++ components/Card.js
@@ -4,7 +4,7 @@ import { HandThumbUpIcon } from '@heroicons/react/24/outline'
 export default function Card({ result }) {
   const BASE_URL="https://image.tmdb.org/t/p/original"
   return(
-    <>
+    <div className='p-3 cursor-pointer hover:text-white active:text-red-400 xl:hover:scale-105 transition-transform duration-200'>
       <Image 
         src={ BASE_URL + result.backdrop_path}
         layout="responsive"
@@ -12,16 +12,16 @@ export default function Card({ result }) {
         height={100}
         alt="Photo"
       />
-      <div className="">
-        <p className="truncate">
+      <div className="p-2">
+        <p className="truncate text-lg">
           { result.overview }
         </p>
-        <h2>{ result.title || result.name }</h2>
-        <p>{ result.release_date || result.first_air_date }
-        <HandThumbUpIcon className='h-5'/>
+        <h2 className='text-lg font-bold'>{ result.title || result.name }</h2>
+        <p className='flex items-center'>{ result.release_date || result.first_air_date }
+        <HandThumbUpIcon className='h-5 ml-3 mr-2'/>
         { result.vote_count }
         </p>
       </div>
-    </>
+    </div>
   )
 }
